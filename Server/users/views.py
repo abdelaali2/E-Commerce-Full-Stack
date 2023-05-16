3d4902fdec32e0c8f6d6dbc020addc8c881a5739
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-05-16 21:19:09 +0300

    Server changes to accept Login func

--- Server/users/views.py
+++ Server/users/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,39 +7,71 @@ from django.contrib.auth.forms import UserCreationForm
 from rest_framework.authtoken.models import Token
 
 
-@api_view(['POST'])
+@api_view(["POST", "OPTIONS"])
 def login_user(request):
-    if request.method == 'POST':
-        username = request.data.get('username')
-        password = request.data.get('password')
+    if request.method == "POST":
+        username = request.data.get("username")
+        password = request.data.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            token = Token.objects.get_or_create(user=user)
+            response = Response({"success": True}, status=status.HTTP_200_OK)
+            response.set_cookie(
+                key="session_token",
+                value=token[0],
+                max_age=settings.SESSION_COOKIE_AGE,
+                httponly=True,
+                secure=True,
+                path="/",
+                samesite="Lax",
+            )
+            return response
         else:
-            return Response({'detail': 'Invalid credentials.'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        
-        
-@api_view(['POST'])
-def logout_user(request):
-    logout(request)
-    return Response({'detail': 'Logged out successfully.'}, status=status.HTTP_200_OK)
+            return Response(
+                {"detail": "Invalid credentials."}, status=status.HTTP_400_BAD_REQUEST
+            )
+    # elif request.method == "OPTIONS":
+    #     response = Response()
+    #     response["allow"] = "POST, OPTIONS"
+    #     return response
 
-@api_view(['POST'])
+
+@api_view(["POST", "OPTIONS"])
+def logout_user(request):
+    if request.method == "POST":
+        logout(request)
+        return Response(
+            {"detail": "Logged out successfully."}, status=status.HTTP_200_OK
+        )
+    elif request.method == "OPTIONS":
+        response = Response()
+        response["allow"] = "POST, OPTIONS"
+        return response
+
+
+@api_view(["POST", "OPTIONS"])
 def register_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.data)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=password)
             login(request, user)
-            return Response({'detail': 'Registration successful.'}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"detail": "Registration successful."}, status=status.HTTP_201_CREATED
+            )
         else:
-            return Response({'detail': 'Registration failed.', 'error': form.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "Registration failed.", "error": form.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    elif request.method == "OPTIONS":
+        response = Response()
+        response["allow"] = "POST, OPTIONS"
+        return response
     else:
         form = UserCreationForm()
-    return Response({'form': form}, status=status.HTTP_200_OK)
+    return Response({"form": form}, status=status.HTTP_200_OK)
